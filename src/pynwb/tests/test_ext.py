b3acf368be17e78f4aaa2c6f02c5a136d7871142
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-05-18 16:36:54 -0400

    Update test_ext.py

--- src/pynwb/tests/test_ext.py
+++ src/pynwb/tests/test_ext.py
@@ -1,24 +1,77 @@
 import os
 from pynwb import NWBHDF5IO, NWBFile
 from datetime import datetime
-from ndx_survey_data import SurveyDataTable
+from ndx_survey_data import NRSDataTable, VASDataTable, MPQDataTable
 from numpy.testing import assert_array_equal
 
 
 def test_ext():
     nwbfile = NWBFile('description', 'id', datetime.now().astimezone())
 
-    survey_data_table = SurveyDataTable(name='survey_data_table',
+    # NRS Survey
+
+    nrs_data_table = NRSDataTable(name='nrs_data_table',
                                               description='desc')
 
-    survey_data_table.add_row(questions='question1', responses='response1')
-    survey_data_table.add_row(questions='question2', responses='response2')
-    survey_data_table.add_row(questions='question3', responses='response3')
-    
+    nrs_data_table.add_row(pain_intensity_rating='2', 
+                           pain_relief_rating='7',
+                           relative_pain_intensity_rating='5',
+                           pain_unpleasantness='9')
+    nrs_data_table.add_row(pain_intensity_rating='1', 
+                           pain_relief_rating='2',
+                           relative_pain_intensity_rating='4',
+                           pain_unpleasantness='3')
+    nrs_data_table.add_row(pain_intensity_rating='8', 
+                           pain_relief_rating='9',
+                           relative_pain_intensity_rating='10',
+                           pain_unpleasantness='7')
+
+    # VAS Survey
+
+    vas_data_table = VASDataTable(name='vas_data_table',
+                                              description='desc')
+
+    vas_data_table.add_row(pain_intensity_rating='20/50', 
+                           pain_relief_rating='12/50',
+                           relative_pain_intensity_rating='45/50',
+                           pain_unpleasantness='9/50')
+    vas_data_table.add_row(pain_intensity_rating='6/50', 
+                           pain_relief_rating='22/50',
+                           relative_pain_intensity_rating='34/50',
+                           pain_unpleasantness='3/50')
+    vas_data_table.add_row(pain_intensity_rating='8/50', 
+                           pain_relief_rating='29/50',
+                           relative_pain_intensity_rating='10/50',
+                           pain_unpleasantness='37/50')
+
+    # MPQ Survey
+
+    mpq_data_table = MPQDataTable(name='mpq_data_table',
+                                              description='desc')
+
+    mpq_data_table.add_row(throbbing='Mild', shooting='Severe', stabbing='Moderate', 
+                           sharp='Moderate', cramping='Severe', gnawing='Mild', 
+                           hot_burning='Mild', aching='Moderate', heavy='Mild', 
+                           tender='Moderate', splitting='Severe', tiring_exhausting='Severe',
+                           sickening='Mild', fearful='Mild', cruel_punishing='Severe')
+    mpq_data_table.add_row(throbbing='Severe', shooting='Severe', stabbing='Mild', 
+                           sharp='Moderate', cramping='Mild', gnawing='Mild', 
+                           hot_burning='Mild', aching='Severe', heavy='Moderate', 
+                           tender='Severe', splitting='Mild', tiring_exhausting='Mild',
+                           sickening='Moderate', fearful='Mild', cruel_punishing='Moderate')
+    mpq_data_table.add_row(throbbing='Moderate', shooting='Mild', stabbing='Severe', 
+                           sharp='Moderate', cramping='Severe', gnawing='Severe', 
+                           hot_burning='Severe', aching='Mild', heavy='Moderate', 
+                           tender='Mild', splitting='Mild', tiring_exhausting='Mild',
+                           sickening='Mild', fearful='Severe', cruel_punishing='Moderate')
+
+
     behavior_module = nwbfile.create_processing_module(name='behavior',
                                                    description='survey/behavioral data')
-    
-    nwbfile.processing['behavior'].add(survey_data_table)
+
+    nwbfile.processing['behavior'].add(nrs_data_table)
+    nwbfile.processing['behavior'].add(vas_data_table)
+    nwbfile.processing['behavior'].add(mpq_data_table)
 
     with NWBHDF5IO('test_nwb.nwb', 'w') as io:
         io.write(nwbfile)
