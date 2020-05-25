00a904580a03a6b04824ae7fa40f605f5b98699f
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-05-25 11:16:45 -0400

    Update survey_data.py

--- src/pynwb/ndx_survey_data/survey_data.py
+++ src/pynwb/ndx_survey_data/survey_data.py
@@ -1,68 +1,23 @@
 from pynwb import register_class
 from pynwb.file import DynamicTable
-from hdmf.utils import docval, call_docval_func, get_docval, getargs
+from hdmf.utils import docval, call_docval_func, get_docval
 
 
-@register_class('NRSDataTable', 'ndx-survey-data')
-class NRSDataTable(DynamicTable):
+@register_class('SurveyDataTable', 'ndx-survey-data')
+class SurveyDataTable(DynamicTable):
     """
-    Table for storing NRS survey data
+    Table for storing survey data
     """
-    __nwbfields__ = ('response_options',)
 
     __columns__ = (
-        {'name': 'pain_intensity_rating', 'description': 'Pain Intensity Rating', 'required': True, 'index': False},
-        {'name': 'pain_relief_rating', 'description': 'Pain Relief Rating', 'required': True, 'index': False},
-        {'name': 'relative_pain_intensity_rating', 'description': 'Relative Pain Intensity Rating', 'required': True,
-         'index': False},
-        {'name': 'pain_unpleasantness', 'description': 'Pain Unpleasantness', 'required': True, 'index': False}
-    )
-
-    @docval(dict(name='name', type=str, doc='name of this NRSDataTable',
-                 default='NRSDataTable'),  # required
-            dict(name='response_options', type=list, doc='Response Options'),
-            dict(name='description', type=str, doc='Description of this VectorData',
-                 default='references the NRS survey table'),
-            *get_docval(DynamicTable.__init__, 'id', 'columns', 'colnames'))
-    def __init__(self, **kwargs):
-        call_docval_func(super(NRSDataTable, self).__init__, kwargs)
-        self.response_options = getargs('response_options', kwargs)
-
-
-@register_class('VASDataTable', 'ndx-survey-data')
-class VASDataTable(DynamicTable):
-    """
-    Table for storing VAS survey data
-    """
-    __nwbfields__ = ('response_options',)
-
-    __columns__ = (
-        {'name': 'pain_intensity_rating', 'description': 'Pain Intensity Rating', 'required': True, 'index': False},
-        {'name': 'pain_relief_rating', 'description': 'Pain Relief Rating', 'required': True, 'index': False},
-        {'name': 'relative_pain_intensity_rating', 'description': 'Relative Pain Intensity Rating', 'required': True,
-         'index': False},
-        {'name': 'pain_unpleasantness', 'description': 'Pain Unpleasantness', 'required': True, 'index': False}
-    )
-
-    @docval(dict(name='name', type=str, doc='name of this VASDataTable',
-                 default='VASDataTable'),  # required
-            dict(name='response_options', type=list, doc='Response Options'),
-            dict(name='description', type=str, doc='Description of this VectorData',
-                 default='references the VAS survey table'),
-            *get_docval(DynamicTable.__init__, 'id', 'columns', 'colnames'))
-    def __init__(self, **kwargs):
-        call_docval_func(super(VASDataTable, self).__init__, kwargs)
-        self.response_options = getargs('response_options', kwargs)
-
-
-@register_class('MPQDataTable', 'ndx-survey-data')
-class MPQDataTable(DynamicTable):
-    """
-    Table for storing MPQ survey data
-    """
-    __nwbfields__ = ('response_options',)
-
-    __columns__ = (
+        {'name': 'nrs_pain_intensity_rating', 'description': 'NRS Pain Intensity Rating', 'required': True, 'index': False},
+        {'name': 'nrs_pain_relief_rating', 'description': 'NRS Pain Relief Rating', 'required': True, 'index': False},
+        {'name': 'nrs_relative_pain_intensity_rating', 'description': 'NRS Relative Pain Intensity Rating', 'required': True, 'index': False},
+        {'name': 'nrs_pain_unpleasantness', 'description': 'NRS Pain Unpleasantness', 'required': True, 'index': False},
+        {'name': 'vas_pain_intensity_rating', 'description': 'VAS Pain Intensity Rating', 'required': True, 'index': False},
+        {'name': 'vas_pain_relief_rating', 'description': 'VAS Pain Relief Rating', 'required': True, 'index': False},
+        {'name': 'vas_relative_pain_intensity_rating', 'description': 'VAS Relative Pain Intensity Rating', 'required': True, 'index': False},
+        {'name': 'vas_pain_unpleasantness', 'description': 'VAS Pain Unpleasantness', 'required': True, 'index': False},
         {'name': 'throbbing', 'description': 'Throbbing', 'required': False, 'index': False},
         {'name': 'shooting', 'description': 'Shooting', 'required': False, 'index': False},
         {'name': 'stabbing', 'description': 'Stabbing', 'required': False, 'index': False},
@@ -80,12 +35,10 @@ class MPQDataTable(DynamicTable):
         {'name': 'cruel_punishing', 'description': 'Cruel-Punishing', 'required': False, 'index': False}
     )
 
-    @docval(dict(name='name', type=str, doc='name of this MPQDataTable',
-                 default='MPQDataTable'),  # required
-            dict(name='response_options', type=list, doc='Response Options'),
+    @docval(dict(name='name', type=str, doc='name of this SurveyDataTable',
+                 default='SurveyDataTable'),  # required
             dict(name='description', type=str, doc='Description of this VectorData',
-                 default='references the MPQ survey table'),
+                 default='references the survey table'),
             *get_docval(DynamicTable.__init__, 'id', 'columns', 'colnames'))
     def __init__(self, **kwargs):
-        call_docval_func(super(MPQDataTable, self).__init__, kwargs)
-        self.response_options = getargs('response_options', kwargs)
+        call_docval_func(super(SurveyDataTable, self).__init__, kwargs)
