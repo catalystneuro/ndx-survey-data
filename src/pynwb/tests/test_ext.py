fc5b24f2e1bf36b5a4447ad52c50f7f3d9cc9815
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-06-01 11:26:20 -0400

    testing NRS

--- src/pynwb/tests/test_ext.py
+++ src/pynwb/tests/test_ext.py
@@ -2,71 +2,42 @@ import os
 from pynwb import NWBHDF5IO, NWBFile
 from datetime import datetime
 from ndx_survey_data import SurveyTable, QuestionResponse
-from numpy.testing import assert_array_equal
+from ndx_survey_data.survey_definitions import nrs_survey_table
 
 
 def test_ext():
 
     nwbfile = NWBFile('description', 'id', datetime.now().astimezone())
 
-    pain_intensity_rating = QuestionResponse(name='pain_intensity_rating',
-                                             description='desc',
-                                             options=['no pain', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                                                      'worst pain'])
-
-    pain_relief_rating = QuestionResponse(name='pain_relief_rating',
-                                          description='desc',
-                                          options=['no pain relief', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                                                   'complete pain relief'])
-
-    relative_pain_intensity_rating = QuestionResponse(name='relative_pain_intensity_rating',
-                                                      description='desc',
-                                                      options=['better', '1', '2', '3', '4', 'same', '6', '7', '8', '9',
-                                                               'worse'])
-
-    pain_unpleasantness = QuestionResponse(name='pain_unpleasantness',
-                                           description='desc',
-                                           options=['pleasant', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                                                    'unpleasant'])
-
-    nrs_survey_table = SurveyTable(name='nrs_survey_table',
-                                   description='desc',
-                                   columns=[
-                                       pain_intensity_rating,
-                                       pain_relief_rating,
-                                       relative_pain_intensity_rating,
-                                       pain_unpleasantness
-                                   ])
-
     nrs_survey_table.add_row(
-        pain_intensity_rating=1,
-        pain_relief_rating=5,
-        relative_pain_intensity_rating=2,
-        pain_unpleasantness=3,
+        nrs_pain_intensity_rating=1,
+        nrs_pain_relief_rating=5,
+        nrs_relative_pain_intensity_rating=2,
+        nrs_pain_unpleasantness=3,
         unix_timestamp=1588217283
     )
 
     nrs_survey_table.add_row(
-        pain_intensity_rating=3,
-        pain_relief_rating=1,
-        relative_pain_intensity_rating=6,
-        pain_unpleasantness=2,
+        nrs_pain_intensity_rating=3,
+        nrs_pain_relief_rating=1,
+        nrs_relative_pain_intensity_rating=6,
+        nrs_pain_unpleasantness=2,
         unix_timestamp=1588217283
     )
 
     nrs_survey_table.add_row(
-        pain_intensity_rating=5,
-        pain_relief_rating=2,
-        relative_pain_intensity_rating=0,
-        pain_unpleasantness=2,
+        nrs_pain_intensity_rating=5,
+        nrs_pain_relief_rating=2,
+        nrs_relative_pain_intensity_rating=0,
+        nrs_pain_unpleasantness=2,
         unix_timestamp=1588217283
     )
 
     nrs_survey_table.add_row(
-        pain_intensity_rating=3,
-        pain_relief_rating=1,
-        relative_pain_intensity_rating=6,
-        pain_unpleasantness=2,
+        nrs_pain_intensity_rating=3,
+        nrs_pain_relief_rating=1,
+        nrs_relative_pain_intensity_rating=6,
+        nrs_pain_unpleasantness=2,
         unix_timestamp=1588217283
     )
 
